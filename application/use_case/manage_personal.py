3dd33a5e5cd30c2076118d402846d5659098afc8
apvasquezo <nanitave@gmail.com>
2025-05-26 13:09:22 -0500

    comit algo

--- application/use_case/manage_personal.py
+++ application/use_case/manage_personal.py
@@ -3,7 +3,6 @@ from domain.entities.Personal import Personal
 from domain.repositories.personal_repository import PersonalRepository
 
 async def create_personal(personal_data: Personal, repository: PersonalRepository)->Personal:
-    print ("estoy en manage ", personal_data)
     return await repository.create_personal(personal_data)
 
 async def get_personal(personal_id: str, repository: PersonalRepository)->Personal:
@@ -12,8 +11,8 @@ async def get_personal(personal_id: str, repository: PersonalRepository)->Person
 async def get_all_personal(repository: PersonalRepository)-> list[Personal]:
     return await repository.get_all_personal()
 
-async def update_personal(personal_data, repository: PersonalRepository)->Personal:
-    return await repository.update_personal(personal_data)
+async def update_personal(personal_id:str, personal_data, repository: PersonalRepository)->Personal:
+    return await repository.update_personal(personal_id, personal_data)
 
 async def delete_personal(personal_id: str, repository: PersonalRepository)->None:
     return await repository.delete_personal(personal_id)
