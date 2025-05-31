52dfafd3ff16b28be4f96177c8bcc506e2b7f127
apvasquezo <nanitave@gmail.com>
2025-05-31 00:44:35 -0500

    docker compose backend

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
 
     DATABASE_URL: str = "postgresql+asyncpg://postgres:risk@postgres_db:5432/riskcontrol" 
+    #DATABASE_URL: str = "postgresql+asyncpg://postgres:risk@localhost:5432/riskcontrol"     
     EMAIL_ADDRESS: str = "test@test.com"
     EMAIL_PASSWORD: str = "1234"   
 
