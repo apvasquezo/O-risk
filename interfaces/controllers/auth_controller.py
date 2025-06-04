4e163c8635a8c7f9b6e4de68abc157153c0f83a0
ElizabethEscobar04 <eescoba5@correo.tdea.edu.co>
2025-06-03 20:19:00 -0500

    update_auth_controller_py

--- interfaces/controllers/auth_controller.py
+++ interfaces/controllers/auth_controller.py
@@ -5,6 +5,7 @@ from domain.repositories.user_repository import UserRepository
 from application.use_case.manage_user import get_user_username
 from infrastructure.database.db_config import get_async_session
 from utils.auth import verify_password, create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["Autenticación"])
 
@@ -18,29 +19,26 @@ class TokenResponse(BaseModel):
     role: str  
 
 @router.post("/login", response_model=TokenResponse)
-async def login(user: UserLogin, db: AsyncSession = Depends(get_async_session)):
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_async_session)):
     repository = UserRepository(db)
-    db_user = await get_user_username(user.username, repository)
+    db_user = await get_user_username(form_data.username, repository)
 
     if not db_user:
         raise HTTPException(status_code=401, detail="Invalid username or password")
 
-    if not verify_password(user.password, db_user.password):
+    if not verify_password(form_data.password, db_user.password):
         raise HTTPException(status_code=401, detail="Invalid username or password")
 
-    # Mapea role_id a nombre legible
     role_map = {1: "super", 2: "admin"}
     role = role_map.get(db_user.role_id)
-    print("role ", role)
     if not role:
         raise HTTPException(status_code=403, detail="Rol no autorizado")
 
-    # Crea el token JWT con rol
     token = create_access_token({
         "sub": db_user.username,
         "role": role
     })
-    print ("token ", token)
+
     return {
         "access_token": token,
         "token_type": "bearer",
