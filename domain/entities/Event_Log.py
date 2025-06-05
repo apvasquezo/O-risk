a15b9d47859e634fbe2d33ea279f07352ec29147
apvasquezo <nanitave@gmail.com>
2025-06-04 20:29:30 -0500

    update event_log

--- domain/entities/Event_Log.py
+++ domain/entities/Event_Log.py
@@ -1,22 +1,25 @@
 from pydantic import BaseModel
 from typing import Optional
-from datetime import datetime
+from datetime import date
 
 class EventLog(BaseModel):
     id_eventlog: Optional[int]=None
     event_id: int
     description:str
-    start_date: datetime 
-    end_date: Optional[datetime] 
-    discovery_date: Optional[datetime] 
-    accounting_date: Optional[datetime]
+    start_date: date
+    end_date: Optional[date] 
+    discovery_date: Optional[date] 
+    accounting_date: Optional[date]
     amount: Optional[float] 
     recovered_amount: Optional[float] 
-    insurance_recovery: Optional[float] 
-    risk_factor_id: int 
+    insurance_recovery: Optional[float]  
     product_id: int 
     process_id: int 
     channel_id: int 
     city: Optional[str] 
     responsible_id: int 
     status: Optional[str] 
+    cause1_id: int
+    cause2_id: Optional[int]
+    conse1_id: int
+    conse2_id: Optional[int]
